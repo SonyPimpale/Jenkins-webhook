bd63a38129f4816918a0eb0ee63f16b57ad8c284
SonyPimpale <pimpale@live.com>
2024-03-22 06:35:04 +0000

    pipeline test

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  count = 2
+  count = 1
   tags= {
     Name = var.tag_name
   }
